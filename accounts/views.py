2aad91f1005fd6b5ccc4a861f9696b51ba4cd0ba
saudNA9 <c3026696@newcastle.ac.uk>
2024-10-02 22:57:08 +0300

    Modified views.py/accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
+from accounts.forms import RegistrationForm
+from config import User, db
 
 accounts_bp = Blueprint('accounts', __name__, template_folder='templates')
 
-@accounts_bp.route('/registration')
-def registration():
-    return render_template('accounts/registration.html')
 
+@accounts_bp.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+
+        if User.query.filter_by(email=form.email.data).first():
+            flash('Email already exists', category="danger")
+            return render_template('accounts/registration.html', form=form)
+
+        new_user = User(email=form.email.data,
+                        firstname=form.firstname.data,
+                        lastname=form.lastname.data,
+                        phone=form.phone.data,
+                        password=form.password.data,
+                        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Account Created', category='success')
+        return redirect(url_for('accounts.login'))
+
+    return render_template('accounts/registration.html', form=form)
 @accounts_bp.route('/login')
 def login():
     return render_template('accounts/login.html')
