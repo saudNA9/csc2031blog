2e27b316b96f48c2ae75419e3ff4b0072a66d136
saudNA9 <c3026696@newcastle.ac.uk>
2024-10-02 22:10:52 +0300

    I have created a new file called forms.py inside accounts package

---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    phone = StringField('Phone', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password', message='Both password fields must be equal!')])
+    submit = SubmitField('Register')
